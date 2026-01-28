dd598c9ec1204aad238a389936c03f80c3a24ed2
Giglo01 <negigitesh@gmail.com>
2026-01-28 22:51:14 +0530

    sd

---
+++ new.js
@@ -0,0 +1,75 @@
+// 1
+// for(let i = 1; i<=100; i++){
+//     console.log(i);
+// }
+
+// 2
+// for(let i=1; i<=10; i++){
+    
+//     console.log(i*i);
+// }
+
+// 3
+// let sum = 0
+// for(let i=1; i<=50; i++){
+//     if(i%2 !==0){
+//         sum+=i
+//     }
+        
+// }
+// console.log(sum);
+
+// 4
+// let a = 12
+// for(let i=1; i<=a; i++){
+//     if(a%i == 0){
+//         console.log(i);
+//     }
+// }
+
+
+// 5
+// for(let i=100; i>=1; i--){
+//     console.log(i);
+// }
+
+
+// 6
+// let a ="nonu"
+// let bag =""
+// for(let i=a.length-1; i>=0; i--){
+//     bag+=a[i]
+// }
+// if(a==bag){
+//     console.log("This is a Palindrome");
+// }
+// else{
+//     console.log("This is not a Palindrome");
+// }
+
+
+// 7
+// let a ="Hello Gitesh"
+// let count = 0
+// for(let i=0; i<a.length; i++){
+//     if(a[i]== 'a' || a[i] == 'e' || a[i] == 'i' || a[i] == 'o' || a[i] == 'u' ){
+//         count++
+//     }
+// }
+// console.log(count);
+
+
+// 8
+// let a = 5
+// for(let i=1; i<=100; i++){
+//     if(i%a==0){
+//         console.log(i);
+//     }
+// }
+
+// 9
+let sum =""
+for(let i=1; i<=4; i++){
+    sum+=i
+    console.log(sum);
+}
